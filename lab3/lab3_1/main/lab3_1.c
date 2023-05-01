6b325d18b88f7984ec60cc1a6536abc470fe887b
rkapur2 <rkapur2@ucsc.edu>
2023-05-01 03:19:25 -0700

    Lab 3 part 1 finished; Lab 3 part 2 work added

--- lab3/lab3_1/main/lab3_1.c
+++ lab3/lab3_1/main/lab3_1.c
@@ -2,6 +2,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/portmacro.h"
 #include "freertos/task.h"
@@ -43,7 +44,7 @@ void IMU_write_reg(uint8_t reg, uint8_t* data, size_t size) {
     i2c_master_write_byte(cmd_handle, reg, true);
     i2c_master_write(cmd_handle, data, size, true);
     i2c_master_stop(cmd_handle);
-    printf("write status: %d\n", i2c_master_cmd_begin(I2C_MASTER_NUM, cmd_handle, 1000 / portTICK_PERIOD_MS));
+    i2c_master_cmd_begin(I2C_MASTER_NUM, cmd_handle, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd_handle);
 }
 
@@ -56,39 +57,54 @@ void IMU_read_reg(uint8_t reg, uint8_t *buf, size_t size) {
     i2c_master_write_byte(cmd_handle, (IMU_ADDR << 1) | I2C_MASTER_READ, true);
     i2c_master_read(cmd_handle, buf, size, I2C_MASTER_LAST_NACK);
     i2c_master_stop(cmd_handle);
-    printf("read status: %d\n", i2c_master_cmd_begin(I2C_MASTER_NUM, cmd_handle, 1000 / portTICK_PERIOD_MS));
+    i2c_master_cmd_begin(I2C_MASTER_NUM, cmd_handle, 1000 / portTICK_PERIOD_MS);
     i2c_cmd_link_delete(cmd_handle);
 }
 
-// Read IMU accelerometer and gyroscope data into pointer arguments
-// Can either read from IMU 1 KB FIFO regsiter OR maunally from on-baprd registers
-void read_IMU(int *accel, int *gyro)
+// Read IMU gyroscope data into pointer arguments
+// Can either read from IMU 1 KB FIFO register OR manually from on-board registers
+// The time between samples for the IMU is stored into tDelta
+void read_IMU(double *gyro_x, double *gyro_y, double *gyro_z, double *tDelta)
 {
     // Write to internal registers
     uint8_t data[1] = {0x0F}; // Choose PWR_MGMT register 0x1F for writing to turn on sensors
-                              // Then turn on accelerometer and gyroscope sensors
-    IMU_write_reg(0x1F, data, 1);
+    const float factor = 250.0f / (1 << 15); // Gyroscope scaling factor depending on range
+    IMU_write_reg(0x1F, data, 1); // Turn on accelerometer and gyroscope sensors
+    data[0] = 0x60 | 0x05; // Set gyroscope range = ±250dps, ODR (sampling rate) = 1.6 kHz
+    *tDelta = 1 / 1600.0f; // Set tDelta to 1/ODR
+    IMU_write_reg(0x20, data, 1);
 
-    // Read accelerometer data and output it
-    uint8_t ax[2];
-    IMU_read_reg(0x0B, &ax[1], 1);
-    IMU_read_reg(0x0C, &ax[0], 1);
-
-    printf("accel: %d\n", (ax[1] << 8) + ax[0]);
-
-    (void)accel;
-    (void)gyro;
+    // Read gyroscope data and output it
+    uint8_t gyro[6];
+    IMU_read_reg(0x11, &gyro[0], 1);
+    IMU_read_reg(0x12, &gyro[1], 1);
+    IMU_read_reg(0x13, &gyro[2], 1);
+    IMU_read_reg(0x14, &gyro[3], 1);
+    IMU_read_reg(0x15, &gyro[4], 1);
+    IMU_read_reg(0x16, &gyro[5], 1);
+    *gyro_x = (int16_t)((gyro[0] << 8) | gyro[1]) * factor;
+    *gyro_y = (int16_t)((gyro[2] << 8) | gyro[3]) * factor;
+    *gyro_z = (int16_t)((gyro[4] << 8) | gyro[5]) * factor;
 }
 
 // Create task
 void task_sensor(void *pvParameter)
 {
-    int accel = 0, gyro = 0;
-    read_IMU(&accel, &gyro); // Throw away initial garbage data
+    double gyro_x, gyro_y, gyro_z, tDelta;
+    double x_angle = 0, y_angle = 0, z_angle = 0;
+    const float gyro_thresh = 1; // ±1 threshold for gyroscope measurements
+    const float angle_thresh = 1; // ±1 threshold for inclination
     while (1) {
-        read_IMU(&accel, &gyro);
-	      ESP_LOGI(TAG, "accel: %d, gyro: %d\n", accel, gyro);
-	      vTaskDelay(2000 / portTICK_PERIOD_MS); // 2 sec delay
+        read_IMU(&gyro_x, &gyro_y, &gyro_z, &tDelta);
+        if (gyro_x > gyro_thresh || gyro_x < -gyro_thresh) x_angle += gyro_x * tDelta;
+        if (gyro_y > gyro_thresh || gyro_y < -gyro_thresh ) y_angle += gyro_y * tDelta;
+        // if (gyro_z > gyro_thresh || gyro_z < -gyro_thresh) z_angle += gyro_z * tDelta; // z angle not needed
+        // ESP_LOGI(TAG, "gyro_x: %.2f, gyro_y: %.2f, gyro_z: %.2f", gyro_x, gyro_y, gyro_z); // Output gyroscope values
+	      // ESP_LOGI(TAG, "x_angle: %.2f, y_angle: %.2f, z_angle: %.2f", x_angle, y_angle, z_angle); // Output angle values
+	      // vTaskDelay(2000 / portTICK_PERIOD_MS); // 2 sec delay
+        ESP_LOGI(TAG, "%s%s",
+            x_angle > angle_thresh ? "DOWN " : x_angle < -angle_thresh ? "UP " : "",
+            y_angle > angle_thresh ? "RIGHT" : y_angle < -angle_thresh ? "LEFT" : ""); // Output inclination direction of board
     }
 }
 
